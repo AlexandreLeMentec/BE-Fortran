3cb93fe91e13d2a6f86023ec7a0ef97207b64304
Alexandre Le Mentec <116634144+AlexandreLeMentec@users.noreply.github.com>
2022-11-28 14:17:22 +0100

    save part 1.2

---
+++ Partie1/Main.f90
@@ -0,0 +1,27 @@
+program pollution
+use m_type
+implicit none 
+    type(phys_type):: phys
+    type(num_type):: num
+    real, dimension(:), allocatable:: mesh
+    real, dimension(:), allocatable :: C1, C2
+    integer::j
+    call read_data (phys,num)
+    allocate(mesh(num%N))
+    allocate(C1(num%N))
+    allocate(C2(num%N))
+    call regular_mesh (phys,num,mesh)
+    call conc_init (phys,num,mesh,C1)
+    call display(phys,num,mesh,C1)
+    open (unit=10, file="resultat.dat")
+    call display(phys,num,mesh,C1)
+    do j=2,num%N
+        call conc_calc(phys,num,C1,C2,j)
+        call display(phys,num,mesh,C2)
+        C1(:)=C2(:)
+    enddo
+    close (10)
+    deallocate(C1)
+    deallocate(C2)
+    deallocate(mesh)
+end program pollution
---
+++ Partie1/sousprog.f90
@@ -0,0 +1,96 @@
+subroutine read_data (phys,num)
+use m_type
+    implicit none
+    type(num_type), intent(inout) :: num
+    type(phys_type), intent(inout) :: phys
+    integer :: i, j
+    open (unit=10, file="donnees.dat")
+    read(10,*)
+    read(10,*) phys%L
+    read(10,*) phys%D
+    read(10,*) phys%C0
+    read(10,*) phys%xd
+    read(10,*) phys%xf
+    write(*,*) "donner le nombre de discrétisation spatial N"
+    read(*,*) num%N
+    write(*,*) "donner le pas du temps Dt"
+    read(*,*) num%Dt
+    write(*,*) "donner la discrétisation temporelle Nt"
+    read(*,*) num%Nt
+    close (10)
+    num%dx = phys%L / (num%N - 1)
+    num%R = phys%D * num%Dt / (num%dx*num%dx)
+end subroutine read_data
+
+function heaviside (x)
+    implicit none
+    real:: heaviside
+    real, intent(in) :: x
+    if (x < 0.) then
+        heaviside = 0.
+    else
+        heaviside = 1.
+    endif
+end function heaviside
+
+subroutine regular_mesh (phys,num,mesh)
+use m_type
+    implicit none
+    type(phys_type), intent(in) :: phys
+    type(num_type), intent(in) :: num
+    real, dimension(num%N), intent(out) :: mesh
+    integer :: i, j
+    do i = 1, num%N
+        mesh(i) = (i - 1) * num%dx
+    enddo
+end subroutine regular_mesh
+
+subroutine conc_init (phys,num,mesh,C1)
+use m_type
+    implicit none
+    type(phys_type), intent(in) :: phys
+    type(num_type), intent(in) :: num
+    real, dimension(num%N), intent(in) :: mesh
+    real, dimension(num%N), intent(out) :: C1
+    real:: heaviside
+    integer :: i
+    do i = 1, num%N
+        C1(i) = phys%C0 *(heaviside(mesh(i) - phys%xd) - heaviside(mesh(i) - phys%xf))
+    enddo
+end subroutine conc_init
+
+subroutine conc_calc (phys,num,C1,C2,j)
+use m_type
+    implicit none
+    type(phys_type), intent(in) :: phys
+    type(num_type), intent(in) :: num
+    real, dimension(num%N), intent(in) :: C1
+    real, dimension(num%N), intent(inout) :: C2
+    integer, intent(in) :: j
+    real::func
+    integer :: i
+    C2(1) = func(j)
+    C2(num%N) = 0.
+    do i = 2, num%N-1
+        C2(i) = num%R*C1(i-1) + (1 - 2*num%R)*C1(i) + num%R*C1(i+1)
+    enddo
+    write(*,*) "C2=",C2
+end subroutine conc_calc
+
+function func (j)
+    implicit none
+    real:: func
+    integer, intent(in) :: j
+    func = 0.
+end function func
+
+subroutine display(phys,num,mesh,C2)
+use m_type
+    implicit none
+    type(phys_type), intent(in) :: phys
+    type(num_type), intent(in) :: num
+    real, dimension(num%N), intent(in) :: mesh
+    real, dimension(num%N), intent(in) :: C2
+    integer :: i
+    write(10,*) C2
+end subroutine display
